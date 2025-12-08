64d5f17c05ca87b3711bb2522265bd398c40b9ea
matthcol <matthias.colin@gmail.com>
2025-12-08 13:39:30 +0100

    create schema

--- sql-init-smalldb/00-create-db-user.sql
+++
@@ -1,6 +0,0 @@
-create user cinema with login password 'password';
-create database dbcinema encoding = 'UTF-8';
-
-\c dbmovie
-
-create schema cinema authorization movie;
---
+++ sql-init-smalldb/00-initdb.sql
@@ -0,0 +1,9 @@
+-- en postgres sur la base de maintenance postgres
+create database dbcinema encoding = 'UTF-8';
+create user cinema with login password 'password'; -- scram-sha-256
+
+-- changement de base
+\c dbcinema
+
+-- creation du schema sc_cinema appartenant à cinema
+create schema sc_cinema authorization cinema;
